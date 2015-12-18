717b7a3b69c323f2428c625eee81586db6456216
Chris James <qui666@gmail.com>
2015-12-18 15:11:02 +0000

    Almost fully rendering endpoints, will need refactor

--- ui/app.elm
+++ ui/app.elm
@@ -10,16 +10,27 @@ import Json.Decode as Decode exposing (Decoder, (:=))
 import Task
 import Effects exposing (Effects, Never)
 import Debug
+import Dict
 
 -- model
 type alias Request = {
-  uri: String
+  uri: String,
+  method: String,
+  headers: Maybe (Dict.Dict String String),
+  body: String
+}
+
+type alias Response = {
+  status: Int,
+  headers: Maybe (Dict.Dict String String),
+  body: String
 }
 
 type alias Endpoint = {
   name: String,
   cdcDisabled: Bool,
-  request: Request
+  request: Request,
+  response: Response
 }
 
 type alias Model = List Endpoint
@@ -43,12 +54,21 @@ decodeEndpoint =
     request = Decode.object1 Request
 
     endpoint =
-        Decode.object3 Endpoint
+        Decode.object4 Endpoint
         ("Name" := Decode.string)
         ("CDCDisabled" := Decode.bool)
         (
-          Decode.object1 Request
+          Decode.object4 Request
           (Decode.at ["Request", "URI"] Decode.string)
+          (Decode.at ["Request", "Method"] Decode.string)
+          (Decode.maybe (Decode.at ["Request", "Headers"] (Decode.dict Decode.string)))
+          (Decode.at ["Request", "Body"] Decode.string)
+        )
+        (
+          Decode.object3 Response
+          (Decode.at ["Response", "Code"] Decode.int)
+          (Decode.maybe (Decode.at ["Response", "Headers"] (Decode.dict Decode.string)))
+          (Decode.at ["Response", "Body"] Decode.string)
         )
   in
       Decode.list endpoint
@@ -69,12 +89,28 @@ renderEndpoint : Endpoint -> Html
 renderEndpoint endpoint = li [] [ div [] [
       h2 [] [(text endpoint.name)],
       code [] [(text "CDC Disabled: "), endpoint.cdcDisabled |> toString |> text],
-      p [] [(text endpoint.request.uri)]
+      h3 [] [(text "Request")],
+      p [] [(text endpoint.request.method), (text " "), (text endpoint.request.uri)],
+      Maybe.withDefault (text "lol") (Maybe.map renderHeaders endpoint.request.headers), -- must be a better way!
+      blockquote [] [(text endpoint.request.body)],
+      h3 [] [(text "Response")],
+      p [] [(endpoint.response.status |> toString |> text)],
+      Maybe.withDefault (text "lol") (Maybe.map renderHeaders endpoint.response.headers), -- must be a better way!
+      blockquote [] [(text endpoint.response.body)]
     ]
   ]
 
 renderEndpoints : List Endpoint -> Html
 renderEndpoints endpoints = ul [] (List.map renderEndpoint endpoints)
+
+renderHeaders : Dict.Dict String String -> Html
+renderHeaders headers =
+  let
+    itemRenderer : (String, String) -> Html
+    itemRenderer (key, value) = li [] [(text key), (text " -> "), (text value)]
+  in
+    ul [] (List.map itemRenderer (Dict.toList headers))
+
 
 -- view
 
